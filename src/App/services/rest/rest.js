7b2e7b37aac2f890089b66de9ae3b75dc04bb5a2
alex <desorbaix@free.fr>
2020-08-19 16:08:14 +0200

    Mise a jour du service rest avec connexion  au store

--- src/App/services/rest/rest.js
+++ src/App/services/rest/rest.js
@@ -1,4 +1,4 @@
-import store from '../../store/store'
+import store from '../../store/redux/redux'
 const ADR_REST_SRV = 'http://localhost:5629';
 //const ADR_REST_SRV = 'http://86.195.10.30:5629';
 
@@ -7,15 +7,31 @@ export const getMessages = (id) => {
     if (undefined !== id) {
         adr += '&id_gte=' + (id + 1);
     }
+    console.log('getMessages')
     fetch(adr)
         .then((fluxReponse) => { return fluxReponse.json() })
         .then((fluxjson) => {
-           // callback(fluxjson);
-           store.dispatch({type:'ADD_MESSAGES',values:fluxjson});
+            if(fluxjson.length>0)
+            store.dispatch({ type: 'ADD_MESSAGES', values: fluxjson })
+            //callback(fluxjson);
             return fluxjson;
         })
 }
-export const postMessage = (objet, callback) => {
+let autoPullingIntervalDescriptor = undefined;
+export const stopAutoPulling = () => {
+    if (undefined !== autoPullingIntervalDescriptor) {
+        clearInterval(autoPullingIntervalDescriptor);
+    }
+}
+export const startAutoPulling = () => {
+    const _store=store;
+    autoPullingIntervalDescriptor=setInterval(monComponent => {
+        let lastID = 0;
+        _store.getState().messages.forEach(arrElem => { if (lastID < arrElem.id) { lastID = arrElem.id } });
+        getMessages(lastID)
+    }, 1000);
+}
+export const postMessage = (objet) => {
     fetch(ADR_REST_SRV + '/public_messages',
         {
             method: 'POST',
@@ -27,38 +43,40 @@ export const postMessage = (objet, callback) => {
     )
         .then((fluxReponse) => { return fluxReponse.json() })
         .then((fluxjson) => {
-            callback(fluxjson);
+            store.dispatch({type:'ADD_MESSAGES',values:[fluxjson]})
             return fluxjson;
         })
 }
-export const getUser = (name, callback, createIfNotExist = true) => {
+export const getUser = (name, createIfNotExist = true) => {
     fetch(`${ADR_REST_SRV}/users?name=${name}`).then(flux => flux.json()).then(obj => {
         if (obj.length === 0 && createIfNotExist) {
-            postUser(name, callback);
+            postUser(name);
         }
-        else { callback(obj[0]); }
+        else { store.dispatch({type:'SET_CURRENT_USER',value:obj[0]}) }
         return obj[0];
     })
 }
-export const getUsers = (callback) => {
+export const getUsers = () => {
     fetch(`${ADR_REST_SRV}/users?_sort=name`).then(flux => flux.json()).then(arr => {
-        callback(arr);
+        //callback(arr);
+        store.dispatch({ type: 'ADD_USERS', values: arr })
         return arr;
     })
 }
-export const postUser = (name, callback) => {
+export const postUser = (name) => {
     fetch(ADR_REST_SRV + '/users',
         {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({name:name})
+            body: JSON.stringify({ name: name })
         }
     )
         .then((fluxReponse) => { return fluxReponse.json() })
         .then((fluxjson) => {
-            callback(fluxjson);
+            // callback(fluxjson);
+            store.dispatch({type:'SET_CURRENT_USER',value:fluxjson})
             return fluxjson;
         })
 }
